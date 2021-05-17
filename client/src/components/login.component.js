49b1e81d383c30f9bcd25f1fe2d37f23979799bb
twothicc <chenenliuu@gmail.com>
2021-05-17 21:53:15 +0800

    login callback done

--- client/src/components/login.component.js
+++ client/src/components/login.component.js
@@ -4,63 +4,20 @@ import { log_in_user, log_out_user } from '../actions/login.action'
 import '../styles/login.css'
 import axios from 'axios'
 
-/**
- * NOTE: you can use log_in_user and log_out_user to set stuff in the initialState of login.reducer
- * Then navbar's button would supposedly change accordingly
- */
 class Login extends Component {
 
-    constructor() {
-        super()
-        this.state = {
-            username: '',
-            password: ''
-        }
-        this.handleUsernameChanged = this.handleUsernameChanged.bind(this)
-        this.handlePasswordChanged = this.handlePasswordChanged.bind(this)
-        this.handleLogin = this.handleLogin.bind(this)
-    }
-
-    handleUsernameChanged(event) {
-        this.setState({
-            username: event.target.value
-        })
-    }
-
-    handlePasswordChanged(event) {
-        this.setState({
-            password: event.target.value
-        })
-    }
-
-    handleLogin() {
-        axios.post("BACKEND URL", {
-            method: 'POST',
-            mode: 'cors',
-            headers: {
-
-            },
-            credentials: 'include',
-            body: "{}"
-        }).then(res => {
-            console.log("YO")
-        })
-    }
-
     render() {
-        const { loggedIn, user, error } = this.props
+        const { loggedIn } = this.props
         return (
-            <div className = 'form-container'>
-                <a href={`https://github.com/login/oauth/authorize?scope=repo&client_id=` + process.env.REACT_APP_CLIENT_ID}>Login with github</a>
+            <div className = 'login-container'>
+                <a href={`https://github.com/login/oauth/authorize?scope=repo&client_id=` + process.env.REACT_APP_CLIENT_ID} class = 'button'>Login with github</a>
             </div>
         )
     }
 }
 
 const mapStateToProps = state => ({
-    loggedIn: state.login.loggedIn,
-    user: state.login.user,
-    error: state.login.error
+    loggedIn: state.login.loggedIn
 })
 
 export default connect(mapStateToProps)(Login)
