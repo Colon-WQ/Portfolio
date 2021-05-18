66a3124fc3e414b9deba20e9ab241e3bef9418bb
Chang Chuan Hao <e0543479@u.nus.edu>
2021-05-18 22:06:05 +0800

    Removed hardcoded domain string

--- server/controllers/logout.controller.js
+++ server/controllers/logout.controller.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { FRONT_END } from '../utils/config.js';
 
 const router = express.Router()
 
@@ -7,7 +8,8 @@ export const logout = async (req, res) => {
     //Domain name subject to changes. Should add to .env
     try {
         console.log("deleting cookie")
-        res.clearCookie("authorization", { domain: 'http://localhost:3000', path: '/'})
+        // domain changed from localhost:3000 to FRONT_END from .env
+        res.clearCookie("authorization", { domain: FRONT_END, path: '/'})
     } catch (err) {
         console.log(error)
         res.status(404).json({ message: error.message });
