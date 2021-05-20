58607f63347702b6c434a63d611793da56a24daa
twothicc <chenenliuu@gmail.com>
2021-05-20 20:44:54 +0800

    improved dashboard ui for testing later

--- client/src/reducers/root.reducer.js
+++ client/src/reducers/root.reducer.js
@@ -1,10 +1,10 @@
 import { combineReducers } from "redux";
 import login from "./login.reducer"
-
-
+import portfolios from "./portfolio.reducer"
 
 
 /**Root reducer combines all reducers into one using combineReducers */
 export default combineReducers({
-  login
+  login,
+  portfolios
 });
