3f9832ea34d058d7342c278f82ee1d25fa785d51
twothicc <chenenliuu@gmail.com>
2021-05-24 21:13:16 +0800

    added swagger docs for express backend

--- server/routes/login-callback.route.js
+++ server/routes/login-callback.route.js
@@ -2,7 +2,88 @@ import express from 'express';
 import { getToken } from '../controllers/github-api.controller.js';
 
 const router = express.Router();
-
+/**
+ * @openapi
+ * tags: 
+ *  name: Login
+ *  description: API to login user using Github Oauth API.
+ * 
+ * components:
+ *  schemas: 
+ *      Login:
+ *          type: object
+ *          properties:
+ *                          client_id:
+ *                              type: string
+ *                              description: Client ID received from Github after registration.
+ *                          client_secret:
+ *                              type: string
+ *                              description: Client Secret received from Github after registration.
+ *                          code:
+ *                              type: string
+ *                              description: Code obtained from https://github.com/login/oauth/authorize?scope=repo&client_id=CLIENT_ID where CLIENT_ID is client id.
+ *                                           Obtained from request as a query parameter.
+ *                          redirect_uri:
+ *                              type: string
+ *                              description: url to redirect request response to.
+ *      User:
+ *          type: object
+ *          properties:
+ *                              data:
+ *                                  type: object
+ *                                  properties:
+ *                                      id: 
+ *                                          type: integer
+ *                                          description: User's unique Github account ID.
+ *                                      login:
+ *                                          type: string
+ *                                          description: User's Github account username.
+ *                                      name:
+ *                                          type: string
+ *                                          description: User's Github account name.
+ *                                      avatar_url:
+ *                                          type: string
+ *                                          description: User's Github avatar image url.
+ *                                      gravatar_id:
+ *                                          type: string
+ *                                          description: User's globally recognized avatar url.
+ *          
+ * /login/authenticate:
+ *  post:
+ *      summary: Obtain an access token Github Oauth API and retrieves user's Github account details.
+ *      tags: [Login]
+ *      description: 1. Using gh code provided by frontend, makes a GET request to Github Oauth API at
+ *                   https://github.com/login/oauth/access_token to obtain an access token for the user. <br /><br />
+ *                   
+ *                   2. Then, a GET request is made to https://api.github.com/user with an Authorization header
+ *                   containing the access token to obtain user's Github account details, which are then returned
+ *                   to the frontend.
+ *      requestBody:
+ *          description: Optional description in *Markdown*
+ *          required: true
+ *          content:
+ *              multipart/form-data:
+ *                  schema: 
+ *                      $ref: '#/components/schemas/Login'                   
+ *      responses: 
+ *          200:
+ *              description: User Github account details.
+ *              content: 
+ *                  application/json:
+ *                      schema: 
+ *                          $ref: '#/components/schemas/User'
+ *                          
+ *          401:
+ *              description: Authorization information is missing or invalid.
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *              
+ */
 router.post('/authenticate', getToken);
 
 export default router;
--- server/server.js
+++ server/server.js
@@ -4,7 +4,10 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
+
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 
 import cookieParser from "cookie-parser";
 import { FRONT_END, BACK_END, SIGN_COOKIE_SECRET, MONGO_URL, PORT } from './utils/config.js';
@@ -14,6 +17,35 @@ import { FRONT_END, BACK_END, SIGN_COOKIE_SECRET, MONGO_URL, PORT } from './util
 import loginRoutes from './routes/login-callback.route.js';
 import portfolioRoutes from './routes/portfolio.route.js';
 import logoutRoutes from './routes/logout.route.js';
+
+
+const swaggerOptions = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Express API for Portfol.io",
+        version: "1.0.0",
+        description:
+          "This is a CRUD API application made with Express and documented with Swagger",
+        license: {
+          name: "MIT",
+          url: "https://spdx.org/licenses/MIT.html",
+        },
+        contact: {
+          name: "Chen En & Chuan Hao"
+        },
+      },
+      servers: [
+        {
+          url: "http://localhost:5000",
+          description: 'Development backend server'
+        },
+      ]
+    },
+    apis: ["./routes/*.js"],
+  };
+  
+const swaggerSpecs = await swaggerJSDoc(swaggerOptions)
 
 const app = express();
 
@@ -39,9 +71,11 @@ app.use(cors(corsOptions));
 // });
 
 // Append routes here
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 app.use('/login', loginRoutes);
 app.use('/portfolio', portfolioRoutes);
 app.use('/logout', logoutRoutes);
+
 
 const CONNECTION_URL = MONGO_URL;
 const PORT_CONFIG = PORT || 5000;
@@ -56,7 +90,7 @@ app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, "..", 'client/deploy/index.html'));
 });
 
-app.listen(PORT_CONFIG, () => console.log("server up and running"));
+app.listen(PORT_CONFIG, () => console.log("server up and running at " + PORT_CONFIG));
 
 // connects mongoose + express
 // mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
