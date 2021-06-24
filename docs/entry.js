d13e6e666823421ab24dae4ed25e8764de69277e
twothicc <chenenliuu@gmail.com>
2021-06-24 21:43:55 +0800

    updated jsdocs for frontend

--- docs/entry.js
+++ docs/entry.js
@@ -25,23 +25,26 @@
   import Component0 from '../src/components/Dashboard.jsx';
 reactComponents['Dashboard'] = Component0;
 
-import Component1 from '../src/components/EntryEditor.jsx';
-reactComponents['EntryEditor'] = Component1;
+import Component1 from '../src/components/DirectoryManager.jsx';
+reactComponents['DirectoryManager'] = Component1;
 
-import Component2 from '../src/components/Home.jsx';
-reactComponents['Home'] = Component2;
+import Component2 from '../src/components/EntryEditor.jsx';
+reactComponents['EntryEditor'] = Component2;
 
-import Component3 from '../src/components/LoginResult.jsx';
-reactComponents['LoginResult'] = Component3;
+import Component3 from '../src/components/Home.jsx';
+reactComponents['Home'] = Component3;
 
-import Component4 from '../src/components/Navbar.jsx';
-reactComponents['Navbar'] = Component4;
+import Component4 from '../src/components/LoginResult.jsx';
+reactComponents['LoginResult'] = Component4;
 
-import Component5 from '../src/components/Portfolio.jsx';
-reactComponents['Portfolio'] = Component5;
+import Component5 from '../src/components/Navbar.jsx';
+reactComponents['Navbar'] = Component5;
 
-import Component6 from '../src/components/Publish.jsx';
-reactComponents['Publish'] = Component6;
+import Component6 from '../src/components/Portfolio.jsx';
+reactComponents['Portfolio'] = Component6;
 
-import Component7 from '../src/components/TemplateSelector.jsx';
-reactComponents['TemplateSelector'] = Component7;
+import Component7 from '../src/components/Publish.jsx';
+reactComponents['Publish'] = Component7;
+
+import Component8 from '../src/components/TemplateSelector.jsx';
+reactComponents['TemplateSelector'] = Component8;
