ffe389700daa62b737a55a25ecd53d0a4d6dfda3
Chang Chuan Hao <e0543479@u.nus.edu>
2021-07-05 21:46:36 +0800

    Temporary fix for draft-js issue

--- client/src/templates/about/AboutTemplateMinimalist.jsx
+++ client/src/templates/about/AboutTemplateMinimalist.jsx
@@ -9,15 +9,15 @@ import { Markup } from 'interweave';
 
 
 const styles = (theme) => ({
-    root: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "start",
-        padding: "0px",
-        alignItems: "center",
-        width: "100%",
-        height: 'auto'
-    },
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "start",
+    padding: "0px",
+    alignItems: "center",
+    width: "100%",
+    height: 'auto'
+  },
 });
 
 const initialTitle = convertToRaw(ContentState.createFromText("About me"))
@@ -25,93 +25,93 @@ const initialTitle = convertToRaw(ContentState.createFromText("About me"))
 const initialContent = convertToRaw(ContentState.createFromText("Describe yourself!"));
 
 const convertToHtml = (htmlString) => {
-    const placeholder = document.createElement('div');
-    placeholder.innerHTML = htmlString;
-    const paragraphs = placeholder.getElementsByTagName("p");
-    if (paragraphs.length === 1) {
-        paragraphs[0].style.marginBlockStart = "0px";
-        paragraphs[0].style.marginBlockEnd = "0px";
-    } else if (paragraphs.length > 1) {
-        paragraphs[0].style.marginBlockStart = "0px";
-        paragraphs[paragraphs.length - 1].style.marginBlockEnd = "0px";
-    }
-    return placeholder.innerHTML;
+  const placeholder = document.createElement('div');
+  placeholder.innerHTML = htmlString;
+  const paragraphs = placeholder.getElementsByTagName("p");
+  if (paragraphs.length === 1) {
+    paragraphs[0].style.marginBlockStart = "0px";
+    paragraphs[0].style.marginBlockEnd = "0px";
+  } else if (paragraphs.length > 1) {
+    paragraphs[0].style.marginBlockStart = "0px";
+    paragraphs[paragraphs.length - 1].style.marginBlockEnd = "0px";
+  }
+  return placeholder.innerHTML;
 }
 
 class AboutTemplateMinimalist extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    static templateName = "Minimalist";
+  static templateName = "Minimalist";
 
-    static info = {
-        fonts: {},
-        colours: { 
-            headerBackgroundFill: {label: "header background fill"},
-            bodyBackgroundFill: {label: "body background fill"}
-        },
-        images: {},
-        texts: {title: {label: "Title"}, about: {label: "Description"}},
-        sections: {}
-    };
+  static info = {
+    fonts: {},
+    colours: {
+      headerBackgroundFill: { label: "header background fill" },
+      bodyBackgroundFill: { label: "body background fill" }
+    },
+    images: {},
+    texts: { title: { label: "Title" }, about: { label: "Description" } },
+    sections: {}
+  };
 
-    static preview = "https://bit.ly/3fwsFKX";
+  static preview = "https://bit.ly/3fwsFKX";
 
-    static script = (index) => "";
+  static script = (index) => "";
 
-    static defaultField = {
-        width: "60%", 
-        height: "80vh", 
-        fonts: {},
-        colours: {
-            headerBackgroundFill: "whitesmoke",
-            bodyBackgroundFill: "#FFFFFF"
-        },
-        images: {},
-        texts: {
-            title: initialTitle,
-            content: initialContent
-        },
-        sections: []
-    };
+  static defaultField = {
+    width: "60%",
+    height: "80vh",
+    fonts: {},
+    colours: {
+      headerBackgroundFill: "whitesmoke",
+      bodyBackgroundFill: "#FFFFFF"
+    },
+    images: {},
+    texts: {
+      title: initialTitle,
+      content: initialContent
+    },
+    sections: []
+  };
 
 
-    render() {
-        const {classes, fields} = this.props; 
-        return (
-            <div 
-                className={classes.root}
+  render() {
+    const { classes, fields } = this.props;
+    return (
+      <div
+        className={classes.root}
+      >
+        <Card
+          style={{ width: '100%', border: 'none', boxShadow: 'none', borderRadius: '0px' }}
+        >
+          <CardContent
+            style={{ padding: '0px' }}
+          >
+            <Typography
+              style={{
+                textAlign: 'center',
+                backgroundColor: fields.colours.headerBackgroundFill,
+              }}
             >
-                <Card
-                    style={{ width: '100%', border: 'none', boxShadow: 'none', borderRadius: '0px' }}
-                >
-                        <CardContent
-                            style={{ padding: '0px' }}
-                        >
-                        <Typography 
-                            style={{
-                                textAlign: 'center',
-                                backgroundColor: fields.colours.headerBackgroundFill, 
-                            }}
-                        >
-                            <Markup noWrap content={convertToHtml(draftToHtml(fields.texts.title))}/>
-                        </Typography>
-                        <Typography 
-                            style={{
-                                backgroundColor: fields.colours.bodyBackgroundFill, 
-                                paddingInline: '10%',
-                            }}
-                        >
-                            <Markup noWrap content={convertToHtml(draftToHtml(fields.texts.content))}/>
-                        </Typography>
-                        
-                        </CardContent>
+              <Markup noWrap content={convertToHtml(draftToHtml(fields.texts.title))} />
+            </Typography>
+            <Typography
+              style={{
+                backgroundColor: fields.colours.bodyBackgroundFill,
+                paddingInline: '10%',
+              }}
+            >
+              <Markup noWrap content={convertToHtml(draftToHtml(fields.texts.content))} />
+            </Typography>
 
-                </Card>
-            </div>
-        )
-    }
+          </CardContent>
+
+        </Card>
+      </div>
+    )
+  }
 }
 
 
